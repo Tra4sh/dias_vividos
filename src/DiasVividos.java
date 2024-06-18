5af07f62247a7b037971b2ba55679cccddbb41ba
Pedro H <pedronionis@gmail.com>
2024-06-18 20:17:21 -0300

    Primeira Versao

---
+++ src/DiasVividos.java
@@ -0,0 +1,9 @@
+public class DiasVividos {
+    public static void main(String[] args) {
+        byte idadeDoUsuario = 19;
+        final short DIAS_DO_ANO = 365;
+        int diasVividos = idadeDoUsuario * DIAS_DO_ANO;
+
+        System.out.println("Você já viveu aproximadamente: " + diasVividos + " dias");
+    }
+}
